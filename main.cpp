9241ae25587accd0e9ff1180ec9297750d81ee19
Salil Mittal <salil.m1311@gmail.com>
2022-08-21 18:11:11 +0530

    use templated tests

--- main.cpp
+++ main.cpp
@@ -1,7 +1,6 @@
-#define USE_CUSTOM_LIST 1
-
+#include <list>
+#include "list.h"
 #include <iostream>
-#include "list_includer.h"
 
 template<typename T>
 void print(std::initializer_list<T> val, char sep = '\t') {
@@ -9,14 +8,15 @@ void print(std::initializer_list<T> val, char sep = '\t') {
 }
 
 template<typename T>
-void pt_list(__list<T>& ls) {
+void pt_list(T& ls) {
 	for(auto x : ls) print({x});
 	std::cout << '\n';
 }
 
-int main() {
-	__list<int> l1 {1, 2, 3};
-	__list<int> l2 = l1;
+template <template <class...> class Container>
+void run_test() {
+	Container<int> l1 {1, 2, 3};
+	Container<int> l2 = l1;
 
 	// l1.push_back(10);
 	// l1.clear();
@@ -30,4 +30,9 @@ int main() {
 
 	pt_list(l1);
 	pt_list(l2);
+}
+
+int main() {
+	run_test<list>();
+	// run_test<std::list>();
 }
