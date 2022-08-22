69c27ae7d26d1ec3e7275ca00b097f5f3e61ca08
cmd05 <splendid.snippet670@slmail.me>
2022-08-22 08:58:09 +0530

    pass iterations from command line

--- main.cpp
+++ main.cpp
@@ -4,8 +4,8 @@
 #include "list/list.h"
 #include "tests/tests.h"
 
-int main() {
-	int n = 50000;
+int main(int argc, char** argv) {
+	int n = atoi(argv[1]);
 	std::cout << "std::list: " << insertion_test<std::list>(n) << " ms\n";
 	std::cout << "list: " << insertion_test<list>(n) << " ms";
 }
