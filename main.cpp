1ca7cf4c99ed4894fd3d3c13c8d6daab29377b34
cmd05 <splendid.snippet670@slmail.me>
2022-08-24 17:34:24 +0530

    too many changes

--- main.cpp
+++ main.cpp
@@ -6,6 +6,9 @@
 
 int main(int argc, char** argv) {
 	int n = atoi(argv[1]);
-	std::cout << "std::list: " << insertion_test<std::list>(n) << " ms\n";
-	std::cout << "list: " << insertion_test<list>(n) << " ms";
+	std::cout << "std::list: " << PerformanceTest<std::list>::run(n) << " ms\n";
+	std::cout << "list: " << PerformanceTest<list>::run(n) << " ms\n";
+
+	// Debug::print_c(list<int>{1,2,3});
+	// FunctionalityTests<list>::test1();
 }
