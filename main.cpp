5d94d8c4806573de970ade17138b76989b1c96f8
cmd05 <salil.m1311@gmail.com>
2022-08-31 01:16:57 +0530

    Remove todo and summary

--- main.cpp
+++ main.cpp
@@ -1,28 +1,6 @@
 #include <iostream>
-
-
-/**
- *	TODO:
- * 		- implement sort()
- * 		- fix merge for empty lists
- * 		- delete pre and sent nodes when invalid
- * 		- validate methods for empty list
- */
-
-/**
- * experimental
- * 
- * SUMMARY:
- * 		- fix merge
- * 		- check methods for empty list
- * 		- delete invalid pre and sent nodes
- * 		- move/copy allocators
- * 		- use std::reverse_iterator
- * 
- * 		- use classes for testing
- */
-
 #include <list>
+
 #include "list/list.h"
 #include "tests/tests.h"
 
