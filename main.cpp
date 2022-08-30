a4abf50d6c9af244013fcdfd9590e3af308c71b9
cmd05 <salil.m1311@gmail.com>
2022-08-31 01:16:23 +0530

    Change spaces to tab

--- main.cpp
+++ main.cpp
@@ -30,5 +30,5 @@ int main(int argc, char** argv) {
 	int n = atoi(argv[1]);
 	std::cout << "std::list: " << PerformanceTest<std::list>::run(n) << " ms\n";
 	std::cout << "list: " << PerformanceTest<list>::run(n) << " ms\n";
-    FunctionalityTests<list>::test1();
+	FunctionalityTests<list>::test1();
 }
