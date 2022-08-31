2581b0f10079ea104ba2c3bd5ddacc4a667b5565
cmd05 <salil.m1311@gmail.com>
2022-08-31 17:25:06 +0530

    add sort test

--- main.cpp
+++ main.cpp
@@ -5,8 +5,8 @@
 #include "tests/tests.h"
 
 int main(int argc, char** argv) {
-	int n = atoi(argv[1]);
-	std::cout << "std::list: " << PerformanceTest<std::list>::run(n) << " ms\n";
-	std::cout << "list: " << PerformanceTest<list>::run(n) << " ms\n";
-	FunctionalityTests<list>::test1();
+	// int n = atoi(argv[1]);
+	// std::cout << "std::list: " << PerformanceTest<std::list>::run(n) << " ms\n";
+	// std::cout << "list: " << PerformanceTest<list>::run(n) << " ms\n";
+	FunctionalityTests<list>::sort_list_test();
 }
